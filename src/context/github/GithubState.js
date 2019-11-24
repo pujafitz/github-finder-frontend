c93fc55a80ede7343fd4a3627a951b726c79e27c
Puja Fitzpatrick <pujafitz@gmail.com>
2019-11-24 13:09:14 -0500

    Moved repos state to context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -49,7 +49,17 @@ const GithubState = props => {
   };
 
   // Get Repos
-
+  const getUserRepos = async username => {
+    setLoading(true);
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      // `https://finder.pujafitz.com/users/${username}/repos?per_page=5&sort=created:asc`
+    );
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    });
+  };
   // Clear Users
   const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
@@ -66,7 +76,8 @@ const GithubState = props => {
         loading: state.loading,
         searchUsers,
         clearUsers,
-        getUser
+        getUser,
+        getUserRepos
       }}
     >
       {props.children}

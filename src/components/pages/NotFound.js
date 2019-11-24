181765a601db5ddcb486caaa90ee6bb7b724a4cd
Puja Fitzpatrick <pujafitz@gmail.com>
2019-11-24 15:35:32 -0500

    Added Home and NotFound functional components

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Oops!</h1>
+      <p className='lead'>This is not the page you are looking for.</p>
+    </div>
+  );
+};
+
+export default NotFound;
